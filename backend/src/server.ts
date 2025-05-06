43d934d6005a4506548eb90963b21b4bad84b156
Adrian <adrianortizzt1vl@gmail.com>
2025-05-06 08:28:10 -0300

    Se añade cors con su configuración básica

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import router from "./routes";
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
